df97110100da9f46805b31ba6afe8059c2207882
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2023-01-06 22:24:04 +0900

    vsc 에서 java 환경 구축 성공 기념

---
+++ January/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("안녕하세요");
+    } 
+}
+
+/* 
+1. cls 입력시 자동으로 클래스 생성
+2. psvm 입력시 자동으로 실행 메서드 생성
+3. 실행은 ctrl+f5(디버깅 없이 실행) or f5 (디버깅 실행) 
+*/
+
