e3e5ea9214e0d0e875c5dea284b396f35885129c
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2023-01-08 21:50:57 +0900

    update

--- 브론즈4/숫자의합.java
+++ 브론즈4/숫자의합.java
@@ -14,7 +14,8 @@ public class 숫자의합 {
         int sum = 0;
         
         for (int i =0; i<N; i++){
-            sum+= a.charAt(i)-'0';
+            // sum+= a.charAt(i)-'0';
+            sum+= a.charAt(i)-48;
         }
         System.out.print(sum);
     }
@@ -29,3 +30,11 @@ public class 숫자의합 {
 출력결과
 15
  */
+
+/*
+✍아스키코드를 활용하여 정수형 데이터를 문자열로 변경할 수 있다.
+
+예를 들어 65라는 정수형 데이터를 문자(string)으로 타입캐스팅 처리한다면 결과는 'A' 간 나온다.
+
+
+  */
