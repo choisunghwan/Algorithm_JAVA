8bedf57943b57cae3f934deae6756938f1fa324e
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2023-01-10 23:06:11 +0900

    최댓값 구하기(배열)

--- Bronze3/최댓값.java
+++ Bronze3/최댓값.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class 최댓값 {
     public static void main(String[] args) {
+
+        //9개의 서로 다른 수가 주어진다. 
         Scanner sc = new Scanner(System.in);
         int[] arr = {sc.nextInt(),sc.nextInt(),sc.nextInt(),
             sc.nextInt(),sc.nextInt(),sc.nextInt(),
@@ -14,7 +16,8 @@ public class 최댓값 {
         int max = 0;
         int index =0;
 
-        for(int value : arr){
+
+        for(int value : arr){ //for(대입받을 변수: 배열명)
             count++;
 
             if(value>max){
